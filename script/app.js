0fd54f98686099249f6a3c0f76293817f15d44ed
PypaertSybrin <sybrin.pypaert@student.howest.be>
2023-09-27 15:11:27 +0200

    password

--- script/app.js
+++ script/app.js
@@ -1,5 +1,20 @@
+// const passwordOptions = ['password', 'text'];
+
 const handlePasswordSwitcher = function () {
     console.log("handlePasswordSwitcher executed");
+    const passwordToggle = document.querySelector('.js-password-toggle-checkbox');
+
+    passwordToggle.addEventListener('change', function () {
+        const passwordInput = document.querySelector('.js-password-input');
+        const checkbox = document.querySelector('.js-password-toggle-checkbox').checked;
+        if(checkbox == true){
+            passwordInput.type = 'text';
+        }
+        else{
+            passwordInput.type = 'password';
+        }
+        // passwordInput.type = passwordOptions[+this.checked];
+    });
 }
 
 const init = function () {
